19fddbe6b5b7a619498801461e662e0f315cb8a5
ori05paz <159430429+ori05paz@users.noreply.github.com>
2024-05-04 03:32:50 +0100

    feat(Menu): Método showMenu en la clase Menu

---
+++ src/org/supermarkets/hiperdino/util/Menu.java
@@ -0,0 +1,12 @@
+package org.supermarkets.hiperdino.util;
+
+
+public class Menu {
+
+    public static int showMenu(){
+
+        String menu = "Menú:\n" + "Seleccione una opción:\n" + "1. Abrir caja\n" + "2. Añadir un nuevo cliente a la cola\n" + "3. Atender un cliente\n" + "4. Ver clientes pendientes\n" + "5. Cerrar supermercado\n";
+    }
+
+    
+}
