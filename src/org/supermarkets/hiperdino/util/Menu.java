b93f605de055793a7531d44e011498764f430940
ori05paz <159430429+ori05paz@users.noreply.github.com>
2024-05-04 03:41:23 +0100

    feat(Menu): Importación de librería javax.swing

--- src/org/supermarkets/hiperdino/util/Menu.java
+++ src/org/supermarkets/hiperdino/util/Menu.java
@@ -1,11 +1,20 @@
 package org.supermarkets.hiperdino.util;
 
+import javax.swing.JOptionPane;
 
 public class Menu {
 
     public static int showMenu(){
 
-        String menu = "Menú:\n" + "Seleccione una opción:\n" + "1. Abrir caja\n" + "2. Añadir un nuevo cliente a la cola\n" + "3. Atender un cliente\n" + "4. Ver clientes pendientes\n" + "5. Cerrar supermercado\n";
+        String menu = "Menú:\n" 
+        + "Seleccione una opción:\n" 
+        + "1. Abrir caja\n" 
+        + "2. Añadir un nuevo cliente a la cola\n" 
+        + "3. Atender un cliente\n" 
+        + "4. Ver clientes pendientes\n" 
+        + "5. Cerrar supermercado\n";
+
+        String option = JOptionPane.showInputDialog(null, menu, "Menú", JOptionPane.PLAIN_MESSAGE);
     }
 
     
