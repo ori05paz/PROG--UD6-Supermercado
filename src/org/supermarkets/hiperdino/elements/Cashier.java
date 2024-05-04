9ffd29800b79d58027209bd7fb60aa12cbd5c37a
ori05paz <159430429+ori05paz@users.noreply.github.com>
2024-05-04 04:13:04 +0100

    feat(Cashier): Creación de getters y setters

--- src/org/supermarkets/hiperdino/elements/Cashier.java
+++ src/org/supermarkets/hiperdino/elements/Cashier.java
@@ -3,10 +3,19 @@ public class Cashier {
     private int number;
     private boolean openedBox;
 
-    
+
     public Cashier(int number, boolean openedBox) {
         this.number = number;
         this.openedBox = openedBox;
+    }
+
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
     }
 
     
