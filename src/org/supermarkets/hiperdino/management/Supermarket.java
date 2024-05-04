c630e604ae63d5e2c3aed908808b7573139b9e5c
ori05paz <159430429+ori05paz@users.noreply.github.com>
2024-05-04 04:39:39 +0100

    feat(Supermarket):  Creación de métodos

--- src/org/supermarkets/hiperdino/management/Supermarket.java
+++ src/org/supermarkets/hiperdino/management/Supermarket.java
@@ -1,6 +1,13 @@
 package org.supermarkets.hiperdino.management;
 
 import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Random;
+import java.util.Stack;
+
+import org.supermarkets.hiperdino.elements.Cashier;
+import org.supermarkets.hiperdino.elements.Client;
+import org.supermarkets.hiperdino.elements.Product;
 
 public class Supermarket {
     
@@ -27,5 +34,54 @@ public class Supermarket {
         this.clientQueue = new LinkedList<>();
     }
 
-    
+    public void openBox() {
+        cashier.openBox();
+    }
+
+    public void addNewClient() {
+        if (cashier.isOpenedBox()) {
+            Client newClients = generateClients();
+            clientQueue.add(newClients);
+            System.out.println("Nuevo cliente añadido a la cola: " + newClients.getName());
+        } else {
+            System.out.println("La caja está cerrada. No se puede agregar un nuevo cliente a la cola.");
+        }
+    }
+
+    public void serveClient() {
+        if (!clientQueue.isEmpty()) {
+            Client nowClient = clientQueue.poll();
+            System.out.println("Atendiendo al cliente: " + nowClient.getName());
+        } else {
+            System.out.println("No hay clientes en la cola.");
+        }
+    }
+
+    public void pendingClients() {
+        if (!clientQueue.isEmpty()) {
+            System.out.println("Clientes pendientes en la cola:");
+            for (Client client : clientQueue) {
+                System.out.println("- " + client.getName());
+            }
+        } else {
+            System.out.println("No hay clientes pendientes en la cola.");
+        }
+    }
+
+    public void closeSupermarket() {
+        cashier.closeBox();
+    }
+
+    private Client generateClients() {
+        String randomName = Client.generateNames();
+        Client client = new Client(randomName);
+        int numProducts = new Random().nextInt(12) + 1;
+        Stack<Product> shoppingBasket = new Stack<>();
+        for (int i = 0; i < numProducts; i++) {
+            Product randomProduct = availableProducts[new Random().nextInt(availableProducts.length)];
+            shoppingBasket.push(randomProduct);
+        }
+        client.setShoppingBasket(shoppingBasket);
+        return client;
+    }
 }
