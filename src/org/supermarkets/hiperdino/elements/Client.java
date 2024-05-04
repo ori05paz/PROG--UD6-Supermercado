d4b90048e6337f4846cd905f3948cad5400a26ab
ori05paz <159430429+ori05paz@users.noreply.github.com>
2024-05-04 03:53:05 +0100

    feat(Client): Creación de getters y setters

--- src/org/supermarkets/hiperdino/elements/Client.java
+++ src/org/supermarkets/hiperdino/elements/Client.java
@@ -4,4 +4,36 @@ public class Client {
     private int id;
     private Stack<Product> shoppingBasket;
     private static int counter = 0;
+    
+    public Client(String name, int id, Stack<Product> shoppingBasket) {
+        this.name = name;
+        this.id = id;
+        this.shoppingBasket = shoppingBasket;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Stack<Product> getShoppingBasket() {
+        return shoppingBasket;
+    }
+
+    public void setShoppingBasket(Stack<Product> shoppingBasket) {
+        this.shoppingBasket = shoppingBasket;
+    }
+
+    
 }
