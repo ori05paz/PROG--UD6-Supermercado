64303ae97cac2e9746566f44976ad35dda710c8d
ori05paz <159430429+ori05paz@users.noreply.github.com>
2024-05-04 04:10:10 +0100

    feat(Cashier): Creación de constructor

--- src/org/supermarkets/hiperdino/elements/Cashier.java
+++ src/org/supermarkets/hiperdino/elements/Cashier.java
@@ -1,6 +1,13 @@
 public class Cashier {
-    
 
     private int number;
     private boolean openedBox;
+
+    
+    public Cashier(int number, boolean openedBox) {
+        this.number = number;
+        this.openedBox = openedBox;
+    }
+
+    
 }
